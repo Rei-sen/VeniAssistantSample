97ed419a15d99c4093309bcb518b102801d508e0
Andy James <andrewj@ksharp.net>
2024-04-23 00:56:31 +0100

    Remove warnings

--- OpenAI/Threads/FunctionCall.cs
+++ OpenAI/Threads/FunctionCall.cs
@@ -9,13 +9,13 @@ public class FunctionCall
     [JsonPropertyName("name")] public required string Name { get; init; }
     [JsonPropertyName("arguments")] public required string Arguments { get; init; }
 
-    public object[] ParseArguments(ParameterInfo[] parameters)
+    public object?[] ParseArguments(ParameterInfo[] parameters)
     {
         var argValues = JsonSerializer.Deserialize<Dictionary<string, object>>(Arguments)
                         ?? throw new Exception("Failed to parse arguments");
 
         return parameters.Select((param, i) =>
-            argValues.TryGetValue(param.Name, out var argValue)
+            argValues.TryGetValue(param.Name!, out var argValue)
                 ? argValue
                 : param.HasDefaultValue
                     ? param.DefaultValue
